1c6a8a42819580524ab474be2ce82e9efebbf99c
efimenko bohdan <bogdane@weblium.com>
2018-05-25 17:42:59 +0300

    add controls for toggle design and change bg

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,50 @@
 Array.from(document.querySelectorAll('.section')).map(section => {
+  const sectionControls = document.createElement('div');
+  sectionControls.className = 'section-controls';
   const invertButton = document.createElement('button');
   invertButton.innerText = 'Invert block';
-  invertButton.className = 'invert-button';
+  invertButton.className = 'invert-button section-controls__button';
 
-  section.insertBefore(invertButton, section.firstChild)
+
+  sectionControls.appendChild(invertButton);
+
+  if(section.getAttribute('data-reserve-bg')) {
+    const changeBgButton = document.createElement('button');
+    changeBgButton.innerText = 'Change bg';
+    changeBgButton.className = 'change-bg-button section-controls__button';
+    sectionControls.appendChild(changeBgButton)
+  }
+
+  section.insertBefore(sectionControls, section.firstChild)
 })
 
 Array.from(document.querySelectorAll('.invert-button')).map(button => {
   button.addEventListener('click', () => {
-    button.parentNode.classList.toggle("dark-block")
+    button.parentNode.parentNode.classList.toggle("dark-block")
   })
+})
+
+Array.from(document.querySelectorAll('.change-bg-button')).map(button => {
+  button.addEventListener('click', () => {
+    const section = button.parentNode.parentNode;
+    const reserveBg = section.getAttribute('data-reserve-bg');
+    const initialBg = section.style.background;
+
+    section.style.background = reserveBg;
+
+    section.setAttribute('data-reserve-bg', initialBg)
+  })
+})
+
+document.querySelector('.toggle-design-button').addEventListener('click', () => {
+  const lastStyleSheet = document.head.children[document.head.children.length - 1];
+
+  if (lastStyleSheet.getAttribute('href') === 'design.css') {
+    document.head.removeChild(lastStyleSheet)
+  }else {
+    const link = document.createElement('link');
+    link.href = 'design.css';
+    link.rel = 'stylesheet';
+    document.head.appendChild(link)
+  }
 })
